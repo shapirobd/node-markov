048fa60e9d01c21d92745284b40fd113e07d19b6
Brian D. Shapiro <briand.shapiro@Brians-MBP.attlocal.net>
2020-11-13 15:52:33 -0500

    step1 (without tests) complete

--- markov.js
+++ markov.js
@@ -18,12 +18,40 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    const chains = {}
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i]
+      if (!chains[word]) {
+        chains[word] = []
+      }
+      if (i === this.words.length - 1) {
+        chains[word].push(null)
+      } else {
+        chains[word].push(this.words[i+1])
+      }
+    }
+    return chains
   }
 
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
-    // TODO
+  makeText(numWords) {
+    let randStart = Math.floor(Math.random() * this.words.length)
+    let nextWord = this.words[randStart]
+    let randText = `${nextWord}`
+    for (let i = 0; i < numWords - 1; i++) {
+      let chains = this.makeChains()
+      let randNext = Math.floor(Math.random() * chains[nextWord].length)
+      nextWord = chains[nextWord][randNext]
+      if (nextWord === null) {
+        break
+      }
+      randText += ` ${nextWord}`
+    }
+    return randText
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat")
+console.log(mm.makeText(100))

d062b2b69fe88ffa4b384ced8d8d2f370420c3fd
Brian D. Shapiro <briand.shapiro@Brians-MacBook-Pro.local>
2020-11-15 18:22:15 -0500

    updated tests

--- markov.test.js
+++ markov.test.js
@@ -1,5 +1,4 @@
 const markov = require('./markov')
-const makeText = require('./makeText')
 let MarkovMachine = markov['MarkovMachine']
 
 let text = 'This is sample text and it is fun to create sample stuff like this'
@@ -16,5 +15,19 @@ describe('makeChains function', () => {
         for (let chain in chains) {
             expect(chains[chain]).toEqual(expect.any(Array))
         }
+    })
+    test('chains have the correct words', () => {
+        expect(chains['This']).toEqual([ 'is' ])
+        expect(chains['is']).toEqual([ 'sample', 'fun' ])
+        expect(chains['sample']).toEqual([ 'text', 'stuff' ])
+        expect(chains['text']).toEqual([ 'and' ])
+        expect(chains['and']).toEqual([ 'it' ])
+        expect(chains['it']).toEqual([ 'is' ])
+        expect(chains['fun']).toEqual([ 'to' ])
+        expect(chains['to']).toEqual([ 'create' ])
+        expect(chains['create']).toEqual([ 'sample' ])
+        expect(chains['stuff']).toEqual([ 'like' ])
+        expect(chains['like']).toEqual([ 'this' ])
+        expect(chains['this']).toEqual([ null ])
     })
 })
